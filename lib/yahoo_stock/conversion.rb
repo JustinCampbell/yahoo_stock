a18fa5a13b4e858031d2a05f689fa288bc76c768
Justin Campbell <cmsjustin@gmail.com>
2011-06-01 17:05:59 -0400

    CONVERSIONS hash constant, date/decimal/integer class methods, tests

--- lib/yahoo_stock/conversion.rb
+++ lib/yahoo_stock/conversion.rb
@@ -3,7 +3,150 @@ module YahooStock
   class Conversion < Base
     
     def self.convert(parameter, data)
-      @@convert ? "Conversion placeholder for #{data}" : data
+      # Return immediately if we're not converting anything
+      return data unless @@convert
+      
+      # If we have a conversion defined for this parameter, convert it, otherwise just return the data
+      if CONVERSIONS[parameter]
+        self.send CONVERSIONS[parameter.to_sym], data
+      else
+        data
+      end
+    end
+    
+    CONVERSIONS = {
+      # :after_hours_change_real_time
+      # :annualized_gain
+      # :ask
+      # :ask_real_time
+      :average_daily_volume => :integer,
+      # :bid
+      # :bid_real_time
+      # :bid_size
+      # :book_value
+      :change => :decimal,
+      # :change_from_200_day_moving_average
+      # :change_from_50_day_moving_average
+      # :change_from_52_week_high
+      # :change_from_52_week_low
+      # :change_in_percent
+      # :change_percent_real_time
+      # :change_real_time
+      # :change_with_percent_change
+      # :commission
+      :day_high => :decimal,
+      :day_low => :decimal,
+      # :day_range
+      # :day_range_real_time
+      # :day_value_change
+      # :day_value_change_real_time
+      :dividend_pay_date => :date,
+      :dividend_per_share => :decimal,
+      :dividend_yield => :decimal,
+      # :earnings_per_share
+      :ebitda => :integer,
+      # :eps_estimate_current_year
+      # :eps_estimate_next_quarter
+      # :eps_estimate_next_year
+      :ex_dividend_date => :date,
+      # :fifty_day_moving_average
+      :fifty_two_week_high => :decimal,
+      :fifty_two_week_low => :decimal,
+      # :fifty_two_week_range
+      # :high_limit
+      # :holdings_gain
+      # :holdings_gain_percent
+      # :holdings_gain_percent_real_time
+      # :holdings_gain_real_time
+      # :holdings_value
+      # :holdings_value_real_time
+      # :last_trade_date
+      # :last_trade_price_only
+      # :last_trade_real_time_with_time
+      # :last_trade_time
+      # :last_trade_with_time
+      # :low_limit
+      :market_capitalization => :integer,
+      # :market_cap_real_time
+      # :more_info
+      # :name
+      # :notes
+      # :one_yr_target_price
+      :open => :decimal,
+      # :order_book_real_time
+      # :peg_ratio
+      # :percent_change_from_200_day_moving_average
+      # :percent_change_from_50_day_moving_average
+      # :percent_change_from_52_week_high
+      # :percent_change_from_52_week_low
+      :previous_close => :decimal,
+      # :price_eps_estimate_current_year
+      # :price_eps_estimate_next_year
+      # :price_paid
+      # :p_e_ratio
+      # :p_e_ratio_real_time
+      # :shares_owned
+      # :short_ratio
+      # :stock_exchange
+      # :symbol
+      # :ticker_trend
+      :trade_date => :date,
+      # :two_hundred_day_moving_average
+      :volume => :integer
+    } unless defined? CONVERSIONS
+    
+    protected
+    
+    # Converts strings to dates. Supports YY before 1970, and dates expressed as MMM dd (Jan  1)
+    def self.date(string)
+      date = string.split('/')
+      month = date[0]
+      day   = date[1]
+      year  = date[2] || Date.today.year.to_s
+      case year.length
+      when 4
+        yyyy = year
+      when 2
+        # If the 2 digit year is in the future, treat it as 19xx
+        yyyy = year > Date.today.year.to_s[2,2] ? "19" << year : "20" << year
+      end
+      
+      # Return properly parsed date
+      Date.parse "#{month}/#{day}/#{yyyy}"
+    end
+    
+    # Converts strings to BigDecimal objects
+    def self.decimal(string)
+      BigDecimal.new string
+    end
+    
+    # Converts strings ending with a large number abbreviation to integers
+    def self.integer(string)
+      
+      # Remove commas from the string
+      string.delete! ","
+      
+      # Check if we're converting from a big number suffix
+      if %w[B M K].include? string[-1,1]
+        
+        # Set the multiplier by the suffix
+        multiplier = case string[-1,1]
+          when "B" then 1_000_000_000
+          when "M" then 1_000_000
+          when "K" then 1_000
+          else 1
+        end
+        
+        # Multiply the remaining string and convert to an integer
+        (BigDecimal.new(string) * multiplier).to_i
+        
+      else
+        
+        # Try to convert the string to an integer
+        string.to_i
+        
+      end
+      
     end
     
   end
---
+++ spec/yahoo_stock/conversion_spec.rb
@@ -0,0 +1,58 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe YahooStock::Conversion do
+  before(:each) do
+    
+  end
+  
+  describe "convert" do
+    
+  end
+  
+  describe "date" do
+    it "should convert dates from MMM dd" do
+      YahooStock::Conversion.date("Jan  1").should == Date.parse("Jan  1 #{Time.now.year}")
+    end
+    
+    it "should convert dates from m/d/yy" do
+      YahooStock::Conversion.date("1/2/03").should == Date.parse("01/02/2003")
+    end
+    
+    it "should convert dates from m/d/yy from before 1970" do
+      YahooStock::Conversion.date("2/4/68").should == Date.parse("02/04/1968")
+    end
+  end
+  
+  describe "decimal" do
+    it "should convert decimals" do
+      YahooStock::Conversion.decimal("123.45").should == 123.45
+    end
+    
+    it "should convert negative decimals" do
+      YahooStock::Conversion.decimal("-123.45").should == -123.45
+    end
+  end
+  
+  describe "integer" do
+    it "should convert billions" do
+      YahooStock::Conversion.integer("1.23B").should == 1_230_000_000
+    end
+    
+    it "should convert millions" do
+      YahooStock::Conversion.integer("1.23M").should == 1_230_000
+    end
+    
+    it "should convert thousands" do
+      YahooStock::Conversion.integer("1.23K").should == 1_230
+    end
+    
+    it "should simply convert others to Fixnum" do
+      YahooStock::Conversion.integer("12345").should == 12_345
+    end
+    
+    it "should convert comma-delimited strings" do
+      YahooStock::Conversion.integer("1,234").should == 1_234
+    end
+  end
+  
+end
